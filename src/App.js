bc57c6a803a459097f0702ca84b25310cab401a4
Thomas Barrett <tsbarrett89@gmail.com>
2020-02-12 16:01:07 -0700

    created user state in app.js

--- src/App.js
+++ src/App.js
@@ -1,11 +1,18 @@
-import React from 'react';
+import React, {useState} from 'react';
 import './App.css';
 import { Route } from 'react-router';
 
-import { CredentialContext } from './contexts/CredentialsContext';
+import { CredentialsContext } from './contexts/CredentialsContext';
 
 
 function App() {
+  const emptyUser = {
+    username: '',
+    user_id: null
+  }
+  const [user, setUser] = useState('emptyUser')
+
+
   return (
     <div className="App">
     </div>
