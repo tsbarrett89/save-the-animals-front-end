1d6675bff2d52b921456158fc856818b69fc5f63
Thomas Barrett <tsbarrett89@gmail.com>
2020-02-13 12:45:55 -0700

    Formik login form

--- src/components/Login.js
+++ src/components/Login.js
@@ -1,7 +1,7 @@
 import React, {useContext, useState} from 'react'
 import axios from 'axios'
 import { Link } from 'react-router-dom'
-import { Formik } from 'formik'
+import { Formik, Form, Field } from 'formik'
 import * as Yup from 'yup'
 
 import { CredentialContext } from '../contexts/CredentialsContext'
@@ -9,7 +9,7 @@ import { CredentialContext } from '../contexts/CredentialsContext'
 
 const Login = () => {
     const { estUser } = useContext(CredentialContext)
-    const [creds, setCreds] = useState({})
+    // const [creds, setCreds] = useState({})
 
 
 
@@ -23,7 +23,7 @@ const Login = () => {
 
                 onSubmit={(values) => {
                     axios
-                        .post(`https://save-the-animals-backend.herokuapp.com/api/users/login`, creds)
+                        .post(`https://save-the-animals-backend.herokuapp.com/api/users/login`, values)
                         .then(res => {
                             console.log(res.data)
                             localStorage.setItem('token', res.data.access_token);
@@ -33,7 +33,48 @@ const Login = () => {
                             console.log(error)
                         })
                 }}
+            validationSchema={Yup.object().shape({
+                username: Yup.string()
+                    .min(6, "Username must be at least 6 characters long.")
+                    .required("We need to know who you are"),
+                password: Yup.string()
+                    .min(6, "Password must be at least 6 characters long.")
+                    .required("We need you to prove who you are.")
+            })}
             >
+                {props => {
+                    const {
+                        values,
+                        touched,
+                        errors,
+                        handleSubmit
+                    } = props
+                }}
+
+                return (
+                    <Form onSubmit={handleSubmit}>
+                        <Field
+                            type='text'
+                            name='username'
+                            placeholder='username'
+                            value={values.username}
+                        />
+                        {touched.username && errors.username && (
+                            <p className='error'>{errors.username}</p>
+                        )}
+                        <Field
+                            type='password'
+                            name='password'
+                            placeholder='password'
+                            value={values.password}
+                        />
+                        {touched.password && errors.password && (
+                            <p className='error'>{errors.username}</p>
+                        )}
+
+                    </Form>
+                )
+
 
             </Formik>
         </div>
