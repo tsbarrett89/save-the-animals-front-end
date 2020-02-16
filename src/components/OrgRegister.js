bfce21103158a8d6f2ea5f2767343ac4cbe5cf0b
Thomas Barrett <tsbarrett89@gmail.com>
2020-02-16 14:19:11 -0700

    set up organization registration form

--- src/components/OrgRegister.js
+++ src/components/OrgRegister.js
@@ -1,9 +1,56 @@
-import React from 'react';
+import React, {useContext} from 'react';
+import axios from 'axios';
+import {Formik, Form, Field} from 'formik';
+import * as Yup from 'yup';
 
-const OrgRegister = () => {
+import {CredentialContext} from '../contexts/CredentialsContext.js';
+
+const OrgRegister = (props) => {
+    const {user} = useContext(CredentialContext)
+
     return (
         <div>
-            <h2>Org Register</h2>
+            <Formik
+                intialValues={{
+                    orgName: '',
+                    user_id: user.user_id
+                }}
+                onSubmit={values => {
+                    axios
+                        .post('https://save-the-animals-backend.herokuapp.com/api/users/register/organizations', values)
+                        .then(res => {
+                            console.log(res.data)
+                            props.history.push('/dashboard')
+                        })
+                        .catch(err => console.log(err))
+                }}
+                validationSchema={Yup.object().shape({
+                    orgName: Yup.string()
+                        .required("We need your official organization name.")
+                })}
+            >
+                {props => {
+                    const {
+                        values,
+                        touched,
+                        errors,
+                        handleSubmit
+                    } = props;
+
+                return (
+                    <Form onSubmit={handleSubmit}>
+                        <h3>Organization Registration</h3>
+                        <Field
+                            type='text'
+                            name='orgName'
+                            placeholder='Organization Name'
+                            value={values.orgName}
+                        />
+                        <button type='submit'>Register Organization</button>
+                    </Form>
+                )
+                }}
+            </Formik>
         </div>
     )
 }
