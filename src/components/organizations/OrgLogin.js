a39feccbc61701befe6452ea380a6542beb954bb
Thomas Barrett <tsbarrett89@gmail.com>
2020-02-22 19:40:46 -0700

    created UserLogin.js

--- src/components/organizations/OrgLogin.js
+++ src/components/organizations/OrgLogin.js
@@ -10,3 +10,5 @@ const OrgLogin = () => {
         </div>
     )
 }
+
+export default OrgLogin
---
+++ src/components/users/UserLogin.js
@@ -0,0 +1,13 @@
+import React, {useState} from 'react';
+import {useForm} from 'react-hook-form';
+
+const UserLogin = () => {
+
+    return (
+        <div>
+            User Login
+        </div>
+    )
+}
+
+export default UserLogin
