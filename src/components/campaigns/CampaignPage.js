b27b20d4299291e5b33e9751f1b79fd7fc5a59d5
Thomas Barrett <tsbarrett89@gmail.com>
2020-02-25 21:18:41 -0700

    started placing elements on CampaignPage

--- src/components/campaigns/CampaignPage.js
+++ src/components/campaigns/CampaignPage.js
@@ -1,11 +1,29 @@
-import React from 'react';
+import React, {useState, useEffect} from 'react';
+import axios from 'axios';
+
+import FundingRecieved from './FundingRecieved';
 
 const CampaignPage = props => {
-    const id = props.match.params.id
+    const id = props.match.params.id;
+    const [campaignInfo, setCampaignInfo] = useState([]);
+
+    useEffect(() => {
+        axios.get(`https://save-the-animals-backend.herokuapp.com/api/campaigns/${id}`)
+        .then(res => {
+            setCampaignInfo(res.data)
+        })
+        .catch(err => console.log(err))
+    }, [])
 
     return (
         <div>
-            <p>Campaign page {id}</p>
+            <h2>{campaignInfo.campaign}</h2>
+            <p>{campaignInfo.location}</p>
+            <p>{campaignInfo.species}</p>
+            <p>{campaignInfo.urgency_level}</p>
+            <p>{campaignInfo.description}</p>
+            <img src={campaignInfo.image} alt={campaignInfo.campaign} />
+            <FundingRecieved id={id} funding={campaignInfo.funding_goal} />
         </div>
     )
 }
