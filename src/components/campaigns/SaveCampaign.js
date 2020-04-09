06d2d225b7ee7545c20fb5c74551b8603a72372a
tsbarrett89 <tsbarrett89@gmail.com>
2020-04-09 14:36:30 -0600

    saveCampaign event in SaveCampaign

--- src/components/campaigns/SaveCampaign.js
+++ src/components/campaigns/SaveCampaign.js
@@ -1,18 +1,23 @@
 import React, { useState, useContext } from 'react'
+import axios from 'axios';
 
 import { UserCredentialContext } from '../../contexts/UserCredentialContext'
 
 const SaveButton = props => {
     const{ user } = useContext(UserCredentialContext)
     const[campaign, setCampaign] = useState({
-        campaign_id: null,
-        user_id: null
+        campaign_id: props.campaign_id,
+        user_id: user.user_id
     })
 
 
     const saveCampaign = e => {
         e.preventDefault()
-        console.log({campaign_id: props.campaign_id, user_id: user.user_id })
+        console.log({ campaign })
+        axios
+            .post(`https://save-the-animals-backend.herokuapp.com/api/users/saved-campaigns`, campaign)
+            .then(res => console.log(res))
+            .catch(err => console.log(err))
     }
 
     return (
